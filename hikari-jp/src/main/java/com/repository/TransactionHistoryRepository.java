c294e134ad7551fafdc552e7905b1167e34eaf0b
Tran Ngoc Thien <thientnde180398@fpt.edu.vn>
2025-04-14 14:25:47 +0700

    a Th push bai sieu dang cap

--- hikari-jp/src/main/java/com/repository/TransactionHistoryRepository.java
+++ hikari-jp/src/main/java/com/repository/TransactionHistoryRepository.java
@@ -3,5 +3,9 @@ package com.repository;
 import com.model.TransactionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
+    List<TransactionHistory> findAllById(Long transactionId);
+
 }
