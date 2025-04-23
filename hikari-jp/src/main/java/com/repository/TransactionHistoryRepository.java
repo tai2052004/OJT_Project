d02c6e7a9975a62add35dbb21cc785c6ff1a5ef6
Tran Ngoc Thien <thientnde180398@fpt.edu.vn>
2025-04-23 13:30:32 +0700

    a Th push bai sua flashcard va quiz result

--- hikari-jp/src/main/java/com/repository/TransactionHistoryRepository.java
+++ hikari-jp/src/main/java/com/repository/TransactionHistoryRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
-    List<TransactionHistory> findAllByUserId(Long transactionId);
+    List<TransactionHistory> findAllByUserId(Long id);
+
 }
